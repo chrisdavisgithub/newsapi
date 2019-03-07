724f84ba9309cc3c3abd24ffacff3523644b5238
RD <christopher.davisroulance@gmail.com>
2019-03-07 15:49:34 +0100

    initial commit

---
+++ precache-manifest.942a7d1b372204f31f3f4d00a7bb3f8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "63487e7e480653428f2c",
+    "url": "/newsapi/static/js/runtime~main.63487e7e.js"
+  },
+  {
+    "revision": "b96f0f0a3aa3c97a05a4",
+    "url": "/newsapi/static/js/main.b96f0f0a.chunk.js"
+  },
+  {
+    "revision": "048aba8373c279d34b76",
+    "url": "/newsapi/static/js/2.048aba83.chunk.js"
+  },
+  {
+    "revision": "b96f0f0a3aa3c97a05a4",
+    "url": "/newsapi/static/css/main.0478e939.chunk.css"
+  },
+  {
+    "revision": "0f8d0655ab30df477411280595c3ba88",
+    "url": "/newsapi/index.html"
+  }
+];
